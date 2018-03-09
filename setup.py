ed7f87d4f7264a470dddbd8c9953e33b4dd03be3
Ben Miller <ben@ben-server.home>
2018-03-08 22:19:54 -0500

    reorg and add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rh',
+    version='0.1',
+    description='Messing around with Robinhood.io',
+    url='https://github.com/bsmiller25/rh',
+    author='Ben Miller',
+    author_email='bsmiller25@gmail.com',
+    packages=['rh'],
+    zip_safe=False)
